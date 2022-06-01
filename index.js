bdb8ca900a34489b601fc9d0804641f8ed47fccf
Calin Lupas <lupas_calin@yahoo.com>
2022-06-01 10:22:38 -0400

    Updates app

--- index.js
+++ index.js
@@ -7,15 +7,35 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    // Load config from .github/config.yml in the repository
+    const config = context.config('config.yml')
+    app.log.info(config);
+
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on("issues.edited", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for editing this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on("issues.closed", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for closing this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("push", async (context) => {
+    app.log.info(context);
+  });
+
+  app.onAny(async (context) => {
+    app.log.info({ event: context.name, action: context.payload.action });
+  });
 };
